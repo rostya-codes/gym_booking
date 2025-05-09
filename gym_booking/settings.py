2925dece98c5a17cec75ee55e70794004a904608
Rostya <rostyamudrik@proton.me>
2025-05-09 15:16:16 +0200

    SECRET KEY

--- gym_booking/settings.py
+++ gym_booking/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from config import SK
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s^_=%8i*+cytytk&$dxbt6l+1)^2(!#l%^%7+i_%f!c029#98_'
+SECRET_KEY = SK
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
